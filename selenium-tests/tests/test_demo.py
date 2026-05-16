ded5decea4a775ad0885e92816949dcd71ef4dcd
DRebyxzS <comoqpzm@gmail.com>
2026-05-15 21:00:05 -0500

    Actualizar Selenium y ajustes del taller

--- selenium-tests/tests/test_demo.py
+++ selenium-tests/tests/test_demo.py
@@ -1,9 +1,4 @@
 def test_open_homepage(driver):
-    url = "http://automationpractice.com/index.php"
+    url = "https://www.saucedemo.com/"
     driver.get(url)
-
-    # Imprimir el título para ver qué devuelve realmente
-    print("TÍTULO OBTENIDO:", driver.title)
-
-    # Comprobamos simplemente que la página carga (título no vacío)
     assert driver.title != ""
--- selenium-tests/tests/test_search.py
+++ selenium-tests/tests/test_search.py
@@ -1,13 +1,8 @@
 from pages.home_page import HomePage
 
-BASE_URL = "http://automationpractice.com/index.php"
+BASE_URL = "https://www.saucedemo.com/"
 
 def test_search_product(driver):
     home = HomePage(driver)
     home.open(BASE_URL)
-
-    # NOTA:
-    # El sitio original de 'My Store' ha cambiado (aparece InMotion Hosting),
-    # por lo que no existe el campo de búsqueda 'search_query_top'.
-    # Este test se deja como placeholder hasta que el profesor defina un nuevo SUT.
     assert driver.title != ""
